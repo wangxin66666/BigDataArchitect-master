e9f6aafefc1286a7e4a8b0bfc0c3161eac6052f0
10225 <wang.xin@datasw.com>
2021-12-27 13:30:27 +0800

    update

--- bigdata-hadoop/src/main/java/com/msb/bigdata/hadoop/wc/Test.java
+++ bigdata-hadoop/src/main/java/com/msb/bigdata/hadoop/wc/Test.java
@@ -2,6 +2,6 @@ package com.msb.bigdata.hadoop.wc;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("ok");
+        System.out.println("ok121");
     }
 }
